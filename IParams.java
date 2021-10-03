ac7b2a3527e7d148355aa0f35dcd5fb2e5db925b
Matty <wu.hang.gcu@qq.com>
2021-10-03 14:02:56 +0800

    create interface1

---
+++ IParams.java
@@ -0,0 +1,15 @@
+package hw2;
+
+import java.util.ArrayList;
+
+public interface IParams {
+
+    int getBigPSpace();
+
+    int getMediumPSpace();
+
+    int getSmallPSpace();
+
+    ArrayList<Integer> getParkingPlan();
+
+}
